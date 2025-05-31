a4a12b6bd5f3ff25db546e593014a4e252595215
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-05-31 19:47:04 -0400

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title:
@@ -83,7 +85,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SpeedInsights />
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
